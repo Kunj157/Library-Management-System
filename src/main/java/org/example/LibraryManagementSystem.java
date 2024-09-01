6675caead763b37e1dc780a56caac47a36e77343
Kunj Patel <kunjr157@gmail.com>
2024-09-01 11:34:05 +0530

    addBookWithNullISBNTest testcase failed

--- src/main/java/org/example/LibraryManagementSystem.java
+++ src/main/java/org/example/LibraryManagementSystem.java
@@ -19,6 +19,7 @@ public class LibraryManagementSystem {
         if(book.getTitle()==null||book.getTitle().trim().isEmpty()){
             throw new IllegalArgumentException("Book Title can't be null");
         }
+
         for (Book availableBook : availableBooks) {
             if (availableBook.getISBN().equals(book.getISBN())) {
                 throw new IllegalArgumentException("Book cannot be added as there is already a book added with ISBN: " + book.getISBN());
